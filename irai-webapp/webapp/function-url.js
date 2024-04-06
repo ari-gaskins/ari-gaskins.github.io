c2e148fe241167c3d52c3435df48443670f6c7ec
Ari Gaskins <arigaskins.tech@gmail.com>
2024-04-05 23:05:04 -0400

    remove AI project

--- irai-webapp/webapp/function-url.js
+++
@@ -1,3 +0,0 @@
-// TODO(reader): Replace with the URL of your Cloud Function
-// NOTE: cloud function will be exported from project where directory function's main.py was imported
-export const CLOUD_FUNCTION_URL = 'https://us-central1-PROJECT_ID.cloudfunctions.net/classify_flower';
